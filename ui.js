6b643d5c5a6d4fab6c2d80c0575bf9fd16dce18e
Tyler Kahn <tkahn6@gmail.com>
2013-10-27 13:48:51 -0400

    integrate new style

--- ui.js
+++ ui.js
@@ -3,10 +3,6 @@ $(document).ready(function() {
         $(this).next().toggle('fast');
     });
 
-    $(document).on('click', '.coupon', function() {
-        alert($(this).data('name'));
-    });
-
     $(document).on('keypress', '#search-bar', function(event) {
         if (event.keyCode === 13)
             $('#search-button').click();
@@ -15,4 +11,6 @@ $(document).ready(function() {
     $(document).on('click', '.navbar-nav a', function() {
         $('#navbar-toggle').click();
     });
+
+    $('.loading:first').hide();
 });
